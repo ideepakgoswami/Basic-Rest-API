4a37c6f254f1f73793127181653a8f8b6e94453d
Deepak <ideepakgoswami@protonmail.com>
2023-03-02 23:59:23 +0530

    Complete API Added with Comments

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
+//REQUIRING PACKAGES
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const bodyParser = require("body-parser");
-const Product = require("./model/models");
 
 
-
+//EXPRESS CONFIG
+const app = express();
 
 //PORT NO.
 PORT = 4500;
-
-
-
-
-// DATABASE CONNECTION
-
-const dbUrl = "mongodb://127.0.0.1:27017/rest_api";
-
-// var connectWithRetry =()=> {
-//     console.log("Called");
-//     return mongoose.connect(dbUrl);
-// };
-// connectWithRetry();
-// mongoose.Promise = global.Promise;
-
-
-mongoose.connect(dbUrl)
-    .then(console.log("connected"))
-    .catch((error) => { console.log("error is: ", error.name) });
-
 
 //BODY-PARSER MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+// MONGODB CONNECTION
+mongoose.connect("mongodb://127.0.0.1:27017/rest_api") //DB NAME:- rest_api(you can use your own)
+    .then(()=>{
+        console.log("connected");
+    })
+    .catch((error) => { console.log("The Error is: ", error.name) });
 
-//API
 
-//CREATE PRODUCT
+// DATABASE SCHEMA
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number
+})
+
+
+// DATABASE MODEL/COLLECTION 
+const Product = new mongoose.model('Product', productSchema);
+
+
+
+//CREATE PRODUCT API
 app.post("/api/v1/product/new", async (req, res) => {
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -48,7 +46,7 @@ app.post("/api/v1/product/new", async (req, res) => {
 })
 
 
-//READ PRODUCT
+//READ PRODUCT API
 app.get("/api/v1/products", async (req, res) => {
     const products = await Product.find();
     res.status(200).json({
@@ -57,53 +55,54 @@ app.get("/api/v1/products", async (req, res) => {
     })
 })
 
-//UPDATE PRODUCT
-app.put("/api/v1/product/:id", async (req,res) => {
+//UPDATE PRODUCT API
+app.put("/api/v1/product/:id", async (req, res) => {
     let product = await Product.findById(req.params.id);
-    if(!product){
+    if (!product) {
         return res.status(500).json({
-            success:false,
-            message:"Product not found"
+            success: false,
+            message: "Product not found"
         })
     }
-    else{
+    else {
         product = await Product.findByIdAndUpdate(req.params.id, req.body);
         res.status(200).json({
-            success:true,
+            success: true,
+            message:"Product Updated Successfully",
             product
         });
     }
-    
-})
+});
 
 
-//DELETE PRODUCT
-app.delete("/api/v1/product/:id", async(req,res)=>{
-    const product=await Product.findById(req.params.id);
+//DELETE PRODUCT API
+app.delete("/api/v1/product/:id", async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-    if(!product){
+    if (!product) {
         res.status(500).json({
-            success:false,
-            message:"Product not found"
+            success: false,
+            message: "Product not found"
         })
     }
-    else{
-    await product.deleteOne();
-    
-    res.status(200).json({
-        success:true
-    })
+    else {
+        await product.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            message:"Product Deleted Successfully"
+        })
     }
 
 
 })
 
 
+
+//HOME PAGE ROUTE(OPTIONAL)
 app.get("/", (req, res) => {
-    res.send("I'm Live");
+    res.send("I'm Home Page Route");
 })
-
-
 
 
 //SERVER LISTENING
